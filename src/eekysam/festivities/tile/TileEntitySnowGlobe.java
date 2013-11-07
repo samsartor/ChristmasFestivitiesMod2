a1894bb89c85099d66161484dc45a510e668de21
Sam Sartor <eeky.sam@gmail.com>
2013-11-07 11:50:51 -0700

    Persistent Snow Globe
    Added TileEntity NBT data to snowglobe

--- src/eekysam/festivities/tile/TileEntitySnowGlobe.java
+++ src/eekysam/festivities/tile/TileEntitySnowGlobe.java
@@ -14,4 +14,23 @@ public class TileEntitySnowglobe extends TileEntity
 		this.worldObj = world;
 		this.type = this.worldObj.rand.nextInt(SnowglobeScene.list.size());
 	}
+	
+	@Override
+	public void writeToNBT(NBTTagCompound tag)
+	{
+		super.writeToNBT(tag);
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound tag)
+	{
+		super.readFromNBT(tag);
+	}
+	
+	public Packet getDescriptionPacket() 
+	{
+    		NBTTagCompound compound = new NBTTagCompound();
+    		this.writeToNBT(compound);
+    		return new Packet132TileEntityData(this.xCoord, this.yCoord, this.zCoord, 1, compound);
+	}
 }
