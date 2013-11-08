4b8e81aa10defff4814aa9a1b36d5ea0c7e5fe1e
Sam Sartor <eeky.sam@gmail.com>
2013-11-07 19:53:07 -0700

    Tried Fix Persistent
    Still not working

--- src/eekysam/festivities/tile/TileEntitySnowglobe.java
+++ src/eekysam/festivities/tile/TileEntitySnowglobe.java
@@ -2,6 +2,7 @@ package eekysam.festivities.tile;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.Packet132TileEntityData;
 import net.minecraft.tileentity.TileEntity;
@@ -10,6 +11,11 @@ import net.minecraft.world.World;
 public class TileEntitySnowglobe extends TileEntity
 {
 	public int type;
+
+	public TileEntitySnowglobe()
+	{
+		
+	}
 	
 	public TileEntitySnowglobe(World world, BlockContainer block)
 	{
@@ -17,25 +23,34 @@ public class TileEntitySnowglobe extends TileEntity
 		this.worldObj = world;
 		this.type = this.worldObj.rand.nextInt(SnowglobeScene.list.size());
 	}
-	
+
 	@Override
 	public void writeToNBT(NBTTagCompound tag)
 	{
+		System.out.println("Saved");
 		super.writeToNBT(tag);
 		tag.setShort("scene", (short) this.type);
 	}
-	
+
 	@Override
 	public void readFromNBT(NBTTagCompound tag)
 	{
+		System.out.println("Read");
 		super.readFromNBT(tag);
 		this.type = tag.getShort("scene");
 	}
-	
-	public Packet getDescriptionPacket() 
+
+	public Packet getDescriptionPacket()
 	{
-    		NBTTagCompound compound = new NBTTagCompound();
-    		this.writeToNBT(compound);
-    		return new Packet132TileEntityData(this.xCoord, this.yCoord, this.zCoord, 1, compound);
+		System.out.println("Desc");
+		NBTTagCompound compound = new NBTTagCompound();
+		this.writeToNBT(compound);
+		return new Packet132TileEntityData(this.xCoord, this.yCoord, this.zCoord, 0, compound);
 	}
+	
+    public void onDataPacket(INetworkManager net, Packet132TileEntityData pkt)
+    {
+    	super.onDataPacket(net, pkt);
+    	this.readFromNBT(pkt.data);
+    }
 }
