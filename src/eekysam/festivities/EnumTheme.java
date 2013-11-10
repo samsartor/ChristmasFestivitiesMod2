d1b5f19ec0be88dbf0af7bf681896cf1a40a0305
Sam Sartor <eeky.sam@gmail.com>
2013-11-09 21:26:55 -0700

    Snowglobe Themes
    More Themes

--- src/eekysam/festivities/EnumTheme.java
+++ src/eekysam/festivities/EnumTheme.java
@@ -29,5 +29,6 @@ public enum EnumTheme
 	Deer,
 	Penguins,
 	CottonCandy,
+	Tree,
 	Magic;
 }
--- src/eekysam/festivities/tile/SnowglobeScene.java
+++ src/eekysam/festivities/tile/SnowglobeScene.java
@@ -10,8 +10,8 @@ import eekysam.festivities.Festivities;
 public class SnowglobeScene
 {
 	public static List<SnowglobeScene> list = new ArrayList<SnowglobeScene>();
-	public static SnowglobeScene SnowTree = new SnowglobeScene("snowWorld", EnumTheme.Snow, EnumTheme.Candycane, EnumTheme.Plants);
-	public static SnowglobeScene CandyWorld = new SnowglobeScene("candyWorld", EnumTheme.Candycane, EnumTheme.Candy, EnumTheme.Snow);
+	public static SnowglobeScene SnowTree = new SnowglobeScene("snowWorld", EnumTheme.Snow, EnumTheme.Candycane, EnumTheme.Plants, EnumTheme.Branches, EnumTheme.Tree, EnumTheme.Leaves);
+	public static SnowglobeScene CandyWorld = new SnowglobeScene("candyWorld", EnumTheme.Candycane, EnumTheme.Candy, EnumTheme.Snow, EnumTheme.Sugar);
 	//public static SnowglobeScene testGrid = new SnowglobeScene("grid");
 			
 	public String texture;
