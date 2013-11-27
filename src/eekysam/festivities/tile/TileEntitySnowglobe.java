0b49ba838e32bb6625587955cc36aa385d4dec11
Sam Sartor <eeky.sam@gmail.com>
2013-11-27 16:19:21 -0700

    Snow Globe Player TP Base
    Basic player itterator

--- src/eekysam/festivities/tile/TileEntitySnowglobe.java
+++ src/eekysam/festivities/tile/TileEntitySnowglobe.java
@@ -1,19 +1,28 @@
 package eekysam.festivities.tile;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cpw.mods.fml.common.network.PacketDispatcher;
 import eekysam.festivities.network.packet.FestPacket;
 import eekysam.festivities.network.packet.PacketUpdateTile;
 import net.minecraft.block.BlockContainer;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.Packet132TileEntityData;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 
 public class TileEntitySnowglobe extends TileEntity
 {
 	public int type = -1;
+	
+	private long ticks;
+	
+	protected List<EntityPlayer> watchers = new ArrayList<EntityPlayer>();
 	
 	public void onChange()
 	{
@@ -54,6 +63,29 @@ public class TileEntitySnowglobe extends TileEntity
 	
     public void updateEntity() 
     {
+    	this.ticks++;
     	
+    	if (ticks % 20 == 0)
+    	{
+    		this.watchers.clear();
+    		
+    		AxisAlignedBB box = AxisAlignedBB.getBoundingBox(this.xCoord - 16, this.yCoord - 16, this.zCoord - 16, this.xCoord + 16, this.yCoord + 16, this.zCoord + 16);
+    		List entities = this.worldObj.getEntitiesWithinAABB(EntityPlayer.class, box);
+    		
+            for (int i = 0; i < entities.size(); ++i)
+            {
+            	EntityPlayer ent = (EntityPlayer) entities.get(i);
+            	
+            	if (this.isLooking(ent))
+            	{
+            		this.watchers.add(ent);
+            	}
+            }
+    	}
+    }
+    
+    protected boolean isLooking(EntityPlayer player)
+    {
+    	return true;
     }
 }
