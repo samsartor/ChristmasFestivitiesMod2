1fd09ab01c1fe1c3acf0f186961489a4bb9d0cef
Sam Sartor <eeky.sam@gmail.com>
2013-11-28 22:00:11 -0700

    Snowglobe TP
    Snowglobes TP player to Kringle

--- src/eekysam/festivities/tile/TileEntitySnowglobe.java
+++ src/eekysam/festivities/tile/TileEntitySnowglobe.java
@@ -4,16 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cpw.mods.fml.common.network.PacketDispatcher;
+import eekysam.festivities.Festivities;
+import eekysam.festivities.kringle.KringleTeleporter;
 import eekysam.festivities.network.packet.FestPacket;
 import eekysam.festivities.network.packet.PacketUpdateTile;
 import net.minecraft.block.BlockContainer;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.Packet132TileEntityData;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.World;
 
 public class TileEntitySnowglobe extends TileEntity
@@ -22,7 +28,13 @@ public class TileEntitySnowglobe extends TileEntity
 	
 	private long ticks;
 	
-	protected List<EntityPlayer> watchers = new ArrayList<EntityPlayer>();
+	protected int lookTicks = 0;
+	protected String looker = "";
+	protected boolean hasLooker = false;
+	
+	public static final int portalTime = 120;
+	
+	//protected List<EntityPlayer> watchers = new ArrayList<EntityPlayer>();
 	
 	public void onChange()
 	{
@@ -65,27 +77,87 @@ public class TileEntitySnowglobe extends TileEntity
     {
     	this.ticks++;
     	
-    	if (ticks % 20 == 0)
+    	if (!this.worldObj.isRemote)
     	{
-    		this.watchers.clear();
-    		
-    		AxisAlignedBB box = AxisAlignedBB.getBoundingBox(this.xCoord - 16, this.yCoord - 16, this.zCoord - 16, this.xCoord + 16, this.yCoord + 16, this.zCoord + 16);
-    		List entities = this.worldObj.getEntitiesWithinAABB(EntityPlayer.class, box);
-    		
-            for (int i = 0; i < entities.size(); ++i)
-            {
-            	EntityPlayer ent = (EntityPlayer) entities.get(i);
-            	
-            	if (this.isLooking(ent))
-            	{
-            		this.watchers.add(ent);
-            	}
-            }
+	    	if (this.hasLooker)
+	    	{
+	    		this.lookTicks++;
+	    	}
+	    	
+	    	if (ticks % 20 == 0)
+	    	{
+	    		boolean flag = false;
+	    		
+	    		AxisAlignedBB box = AxisAlignedBB.getBoundingBox(this.xCoord - 16, this.yCoord - 16, this.zCoord - 16, this.xCoord + 16, this.yCoord + 16, this.zCoord + 16);
+	    		List entities = this.worldObj.getEntitiesWithinAABB(EntityPlayer.class, box);
+	    		
+	            for (int i = 0; i < entities.size(); ++i)
+	            {
+	            	EntityPlayer player = (EntityPlayer) entities.get(i);
+	            	
+            		EntityPlayerMP playermp = (EntityPlayerMP) player;
+            		
+	            	if (this.isLooking(player) && this.canSee(player))
+	            	{
+	            		if (this.hasLooker)
+	            		{
+	            			if (this.looker.equals(player.getEntityName()))
+	            			{
+	            				flag = true;
+	            				if (this.lookTicks > this.portalTime)
+	            				{
+	            					MinecraftServer mServer = MinecraftServer.getServer();
+	            					playermp.mcServer.getConfigurationManager().transferPlayerToDimension(playermp, Festivities.kringleId, new KringleTeleporter(mServer.worldServerForDimension(Festivities.kringleId)));
+	            					
+	            					flag = false;
+	            				}
+	            				break;
+	            			}
+	            		}
+	            		else
+	            		{
+	            			this.looker = player.getEntityName();
+	            			this.lookTicks = 0;
+	            			this.hasLooker = true;
+	            			flag = true;
+	            			break;
+	            		}
+	            	}
+	            }
+	            
+	            if (!flag)
+	            {
+	            	this.hasLooker = false;
+	            	this.lookTicks = 0;
+	            }
+	            else
+	            {
+	            	
+	            }
+	    	}
     	}
     }
     
     protected boolean isLooking(EntityPlayer player)
     {
-    	return true;
+        Vec3 look = player.getLook(1.0F).normalize();
+        Vec3 pos = this.worldObj.getWorldVec3Pool().getVecFromPool(this.xCoord - player.posX + 0.D, this.yCoord - (player.posY + (double)player.getEyeHeight()) + 0.5D, this.zCoord - player.posZ + 0.5D);
+        double dist = pos.lengthVector();
+        pos = pos.normalize();
+        double stare = look.dotProduct(pos);
+        
+    	return stare > 1.0D - 0.025D / dist;
+    }
+    
+    protected boolean canSee(EntityPlayer player)
+    {
+    	Vec3 look = player.getLook(1.0F).normalize();
+    	return this.worldObj.clip(this.worldObj.getWorldVec3Pool().getVecFromPool(this.xCoord + 0.5F + look.xCoord * -2, this.yCoord + 0.5F + look.yCoord * -2, this.zCoord + 0.5D + look.zCoord * -2), this.worldObj.getWorldVec3Pool().getVecFromPool(player.posX, player.posY + (double)player.getEyeHeight(), player.posZ)) == null;
+    }
+    
+    @Deprecated
+    public boolean rayTraceTo(float dist, EntityLivingBase entity)
+    {
+    	return false;//entity.rayTrace(dist, 1.0F);
     }
 }
